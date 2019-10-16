eed55da0ead24f2cb56529d31118ea84f3430f7a
Sreekanth Josyula <Sreekanth.Josyula@thomsonreuters.com>
2019-10-16 18:04:11 +0300

    Order API changes

--- EventsApp.Orders.API/Startup.cs
+++ EventsApp.Orders.API/Startup.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EventInfo.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using OrderInfo.Business;
+using OrderInfo.Business.Services;
 
 namespace EventsApp.Orders.API
 {
@@ -26,6 +30,11 @@ namespace EventsApp.Orders.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDbContext<EventsDBContext>(o =>
+                o.UseSqlServer(Configuration["connectionStrings:DefaultConnection"]));
+            services.AddTransient<ITicketOrderService, TicketOrderService>();
+            services.AddTransient<IAutoMapper, AutoMapping>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
